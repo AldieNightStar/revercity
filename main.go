9a05caa14493e23021c33e482ec2f4fd7998cbe8
AldieNightStar <aldie.nightstar@gmail.com>
2024-05-20 01:45:46 +0200

    Main now accepts args for address and port

--- main.go
+++ main.go
@@ -2,20 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/AldieNightStar/revercity/revercity"
 )
 
 func main() {
-	var connector = revercity.NewTcpConnector("localhost:25565")
-	var c, err = revercity.Serve(7777, connector)
+	var args = os.Args[1:]
+	if len(args) < 2 {
+		fmt.Println("Enter [connect address] [new port]")
+		return
+	}
+
+	address := args[0]
+	newPort, err := strconv.Atoi(args[1])
+	if err != nil {
+		fmt.Println("Wrong new port")
+		return
+	}
+
+	connector := revercity.NewTcpConnector(address)
+	c, err := revercity.Serve(newPort, connector)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	// Logging
 	for {
 		fmt.Println(c)
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 5)
 	}
 }
